e019b06f258f1738492b57105353ba14e275f40e
Ngweyee <ngweyeeshoon.1@gmail.com>
2019-06-13 16:17:57 +0800

    - Suspending functions

--- app/src/main/java/com/nys/androiddeveloper/coroutinesample/MainActivity.kt
+++ app/src/main/java/com/nys/androiddeveloper/coroutinesample/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.delay
 
 const val TAG: String = "MainActivity"
 
@@ -31,7 +31,7 @@ class MainActivity : AppCompatActivity() {
 
         //Million Coroutine
 
-        val deferred = (1..1_000_000).map { n ->
+     /*   val deferred = (1..1_000_000).map { n ->
             GlobalScope.async {
                 n
             }
@@ -40,7 +40,19 @@ class MainActivity : AppCompatActivity() {
         runBlocking {
             val sum = deferred.sumBy { it.await() }
             println("Sum : $sum")
+        }*/
+
+        GlobalScope.async {
+            println(workload(12))
         }
 
+
+    }
+
+
+
+    suspend fun workload(n: Int): Int {
+        delay(3000)
+        return n
     }
 }
